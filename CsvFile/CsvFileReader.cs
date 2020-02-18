beebcebfe06026a734ae9b16842f1dc13c44e2b7
Sachek Denis <SachekDenis>
2020-02-18 18:13:55 +0300

    Revert "fixed file structure"
    This reverts commit dd11090552a676eebff9880b3ef76143260ee7f2.

---
+++ CsvFile/CsvFileReader.cs
@@ -0,0 +1,50 @@
+﻿using CsvHelper;
+using Logger;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace FileReaders
+{
+    public class CsvFileReader : IReader
+    {
+        public IEnumerable<StudentInfo> ReadFile(string path)
+        {
+            if (path == string.Empty || path == null)
+                throw new ArgumentNullException();
+
+            var records = new List<StudentInfo>();
+
+            try
+            {
+                using (var reader = new StreamReader(path))
+                using (var csv = new CsvReader(reader, CultureInfo.CurrentCulture))
+                {
+                    csv.Read();
+                    csv.ReadHeader();
+
+                    List<string> subjectNames = csv.Context.HeaderRecord.Skip(2).ToList();
+
+                    while (csv.Read())
+                    {
+                        StudentInfo studentInfo = new StudentInfo();
+                        studentInfo.FirstName = csv.GetField<string>(0);
+                        studentInfo.Surname = csv.GetField<string>(1);
+                        studentInfo.Marks = new List<SubjectMark>();
+                        subjectNames.ForEach(name => studentInfo.Marks.Add(new SubjectMark() { Subject = name, Mark = csv.GetField<int>(name) }));
+                        records.Add(studentInfo);
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+
+            return records;
+        }
+    }
+}
---
+++ FileWriters/ExcelWriter.cs
@@ -0,0 +1,47 @@
+﻿using Logger;
+using Model;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FileWriters
+{
+    public class ExcelWriter : IWriter
+    {
+        public void WriteToFile(GroupReport information, string path)
+        {
+            if (information == null || path == null)
+                throw new ArgumentNullException();
+
+            try
+            {
+                FileInfo excelFile = new FileInfo(path);
+
+                using (ExcelPackage package = new ExcelPackage(excelFile))
+                {
+                    var worksheet = package.Workbook.Worksheets.Add($"{typeof(SummaryMarkInfo).ToString()}{package.Workbook.Worksheets.Count}");
+
+                    var range = worksheet.Cells[1, 1];
+
+                    range.LoadFromCollection(information.StudentAvegareInfos, true);
+
+                    range.AutoFitColumns();
+
+                    var lastRow = worksheet.Dimension.End.Row;
+
+                    range = worksheet.Cells[lastRow + 2, 1];
+
+                    range.LoadFromCollection(information.SummaryMarkInfo.Marks, true);
+
+                    package.Save();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
