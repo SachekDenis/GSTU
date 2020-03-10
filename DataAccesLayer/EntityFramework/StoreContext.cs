bbb0b778a304a8c81816e2dde21e9de30af23a58
SachekDenis <sachek.denis@gmail.com>
2020-03-10 22:58:59 +0300

    created EF core context

---
+++ DataAccesLayer/EntityFramework/StoreContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace DataAccesLayer.EntityFramework
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["StoreConnection"].ConnectionString);
+        }
+    }
+}
