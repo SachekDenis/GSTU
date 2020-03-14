a7e127ba1d340cc559dc7ec0762abbc320cf9820
SachekDenis <sachek.denis@gmail.com>
2020-03-14 18:17:15 +0300

    added more models

---
+++ DataAccesLayer/Models/Motherboard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccesLayer.Models
+{
+    public class Motherboard:Characteristics
+    {
+        public string Socket { get; set; }
+
+        public string Chipset { get; set; }
+
+        public string RamType { get; set; }
+
+        public int RamCount { get; set; }
+
+        public string ExtensionSlots { get; set; }
+
+        public string Audio { get; set; }
+
+        public string Ethernet { get; set; }
+
+        public string Interfaces { get; set; }
+    }
+}
