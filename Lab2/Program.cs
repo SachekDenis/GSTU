90f64102e5c8eff8fb81ffc28ea8b579f0e69453
SachekDenis <sachek.denis@gmail.com>
2020-03-14 13:46:06 +0300

    added initial model

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using DataAccesLayer.EntityFramework;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
 
 namespace Lab2
 {
@@ -6,6 +11,21 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+            var config = builder.Build();
+
+            var services = new ServiceCollection()
+                .AddDbContext<StoreContext>(options =>
+                {
+                    options.UseSqlServer(config.GetConnectionString("StoreConnection"));
+                })
+                .BuildServiceProvider();
+
+            var context = services.GetService<StoreContext>();
+
+
             Console.WriteLine("Hello World!");
         }
     }
