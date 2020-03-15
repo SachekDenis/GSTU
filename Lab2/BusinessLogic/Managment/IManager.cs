08f2b9bacbb440a0446b38f6340d00387b207f3c
SachekDenis <sachek.denis@gmail.com>
2020-03-15 22:04:59 +0300

    added business logic

---
+++ Lab2/BusinessLogic/Managment/IManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic.Managment
+{
+    public interface IManager<T>
+        where T : class
+    {
+        T GetById(int itemId);
+
+        void Add(T item);
+
+        void Delete(int itemId);
+
+        void Update(T item);
+
+        IEnumerable<T> GetAll();
+    }
+}
