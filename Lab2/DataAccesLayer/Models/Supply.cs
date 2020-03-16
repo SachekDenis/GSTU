c840350e4ef16d6bea9cf8cb72ce301105ae7938
SachekDenis <sachek.denis@gmail.com>
2020-03-16 23:04:34 +0300

    added update and delete logic

--- Lab2/DataAccesLayer/Models/Supply.cs
+++ Lab2/DataAccesLayer/Models/Supply.cs
@@ -7,6 +7,7 @@ namespace DataAccesLayer.Models
     public class Supply
     {
         public int Id { get; set; }
+        public int SupplierId { get; set; }
         public Supplier Supplier { get; set; }
         public DateTime Date { get; set; }
     }
