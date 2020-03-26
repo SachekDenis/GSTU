f4f76b8ba47a630d0a6c8fe3607a522c7abcd40f
SachekDenis <sachek.denis@gmail.com>
2020-03-26 13:40:02 +0300

    added json reader

---
+++ Lab2/DataAccesLayer/File/JsonReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DataAccesLayer.File
+{
+    public class JsonReader<T> : IReader<T>
+    {
+        public T Read(string filename)
+        {
+            using (var reader = new StreamReader(filename))
+            {
+                var json = reader.ReadToEnd();
+                return JsonSerializer.Deserialize<T>(json);
+            }
+        }
+    }
+}
