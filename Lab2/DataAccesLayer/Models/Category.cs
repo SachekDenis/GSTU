930c6a8bffc7798376598c78fe7279a3a33c7f78
SachekDenis <sachek.denis@gmail.com>
2020-03-31 11:20:38 +0300

    fixed branch structure

--- Lab2/DataAccesLayer/Models/Category.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DataAccessLayer.Models
-{
-    public class Category : IEntity
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- Lab2/DataAccesLayer/Models/Characteristic.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataAccessLayer.Models
-{
-    public class Characteristic : IEntity
-    {
-        public int Id { get; set; }
-        public int CategoryId { get; set; }
-        public string Name { get; set; }
-    }
-}
