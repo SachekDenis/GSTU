4167de7a040382ceb4674c0ce28ca2cea77c4a38
SachekDenis <sachek.denis@gmail.com>
2020-05-17 16:41:17 +0300

    fixed pages folders

--- Lab3/ComputerStore.WebUI/Pages/Identity/Account/Logout.cshtml.cs
+++ Lab3/ComputerStore.WebUI/Pages/Identity/Account/Logout.cshtml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using ComputerStore.DataAccessLayer.Models.Identity;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -9,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace ComputerStore.WebUI.Areas.Identity.Pages.Account
+namespace ComputerStore.WebUI.Pages.Identity.Account
 {
     [AllowAnonymous]
     public class LogoutModel : PageModel
